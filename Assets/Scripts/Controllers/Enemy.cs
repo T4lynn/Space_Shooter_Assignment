a7fbb55a69caa3bcfa92d08e2f91c43c046670ab
T4lynn <148375548+T4lynn@users.noreply.github.com>
2024-11-09 15:38:20 -0500

    finished tweaking task 1

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    bool Direction = false;
+    float EnemySpeed = 3;
+    bool Coroutinerunning = false;
+    private void Update()
+    {
+        Strafe();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        StartCoroutine(EnemyWarp());
+        if (!Coroutinerunning)
+        {
+            StartCoroutine(EnemyWarp());
+        }
     }
 
     IEnumerator EnemyWarp()
     {
+        Coroutinerunning = true;
+        Debug.Log("Coroutine started");
         float NewPositionX;
         float NewPositionY;
         NewPositionX = Random.Range(-10, 10);
@@ -23,5 +35,27 @@ public class Enemy : MonoBehaviour
         NewPositionY = Random.Range(-10, 10);
         NewVectorPosition = new Vector3(NewPositionX, NewPositionY, 0);
         transform.position = NewVectorPosition;
+        Coroutinerunning = false; 
+        yield break;
+    }
+
+    void Strafe()
+    { 
+        if (!Direction)
+        {
+            transform.position = transform.position + Vector3.left  * Time.deltaTime * EnemySpeed; 
+            if (transform.position.magnitude >= 10)
+            {
+                Direction = true;
+            }
+        }
+        else
+        {
+            transform.position = transform.position + Vector3.right * Time.deltaTime * EnemySpeed;
+            if (transform.position.magnitude >= 10)
+            {
+                Direction = false;
+            }
+        }
     }
 }
