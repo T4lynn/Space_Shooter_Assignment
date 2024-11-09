695655c11854a91b6f0b70416db0c68297ca6be2
T4lynn <148375548+T4lynn@users.noreply.github.com>
2024-11-09 13:23:13 -0500

    added project, added warping.

---
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(EnemyWarp());
+    }
+
+    IEnumerator EnemyWarp()
+    {
+        float NewPositionX;
+        float NewPositionY;
+        NewPositionX = Random.Range(-10, 10);
+        NewPositionY = Random.Range(-10, 10);
+        Vector3 NewVectorPosition = new Vector3(NewPositionX, NewPositionY, 0);
+        yield return new WaitForSeconds(1);
+        Debug.Log("New Position" + NewVectorPosition);
+        transform.position = NewVectorPosition;
+        yield return new WaitForSeconds(2);
+        NewPositionX = Random.Range(-10, 10);
+        NewPositionY = Random.Range(-10, 10);
+        NewVectorPosition = new Vector3(NewPositionX, NewPositionY, 0);
+        transform.position = NewVectorPosition;
+    }
+}
